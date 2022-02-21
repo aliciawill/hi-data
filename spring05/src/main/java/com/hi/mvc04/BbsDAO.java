3549361aca783a273c4c83c7a93ae64048c06646
lim jungwon <jungwonalicia@gmail.com>
2022-02-21 11:41:48 +0900

    myBatis

--- spring05/src/main/java/com/hi/mvc04/BbsDAO.java
+++ spring05/src/main/java/com/hi/mvc04/BbsDAO.java
@@ -1,5 +1,7 @@
 package com.hi.mvc04;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,5 +13,18 @@ public class BbsDAO {
 	public int create(BbsVO vo) {
 		return my.insert("bbs.create", vo);
 		//namespace.id
+	}
+	public BbsVO one(BbsVO vo) {
+		return my.selectOne("bbs.one", vo);
+	}
+	
+	public List<BbsVO> all() {
+		return my.selectList("bbs.all");
+	}
+	public int update(BbsVO vo) {
+		return my.update("bbs.up", vo);
+	}
+	public int delete(BbsVO vo) {
+		return my.delete("bbs.del", vo);
 	}
 }
