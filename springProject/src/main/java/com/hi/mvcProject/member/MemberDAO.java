b121ffd7fb806156dfb85dac7ae45f8411eba4bb
aliciawill <aliciawill@kakao.com>
2022-04-25 11:44:46 +0900

    기본형변환

--- springProject/src/main/java/com/hi/mvcProject/member/MemberDAO.java
+++ springProject/src/main/java/com/hi/mvcProject/member/MemberDAO.java
@@ -1,12 +1,5 @@
 package com.hi.mvcProject.member;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
