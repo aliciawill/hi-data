2eaed4e72f50bd53b29c6777236c53591341c23a
aliciawill <aliciawill@kakao.com>
2022-04-25 10:29:32 +0900

    인터페이스

--- springProject/src/main/java/com/hi/mvcProject/high/BbsDAOInterface.java
+++ springProject/src/main/java/com/hi/mvcProject/high/BbsDAOInterface.java
@@ -8,8 +8,17 @@ public interface BbsDAOInterface {
 	//crud 메서드를 정의!
 	//인터페이스를 implements해서
 	//BbsDAOImple 클래스를 구현해보세요.
-	public int insert(BbsVO vo);
+	//인터페이스에 기능을 정의할 목적으로 사용하는 메서드는
+	//일반적인 메서드가 아님.
+	//구체적인 구현이 빠져있음.
+	//불완전한 메서드의 형태임.
+	//구현이 빠져있는 불완전한 메서드: 추상메서드 
+	//인터페이스에는 추상메서드만 써야함.!
+	//abstract단어는 생략가능!
+	//인터페이스에서 사용하는 메서드는 모두다 public!
+	//public abstract 생략가능!!
+	public abstract int insert(BbsVO vo);
 	public int update(BbsVO vo);
 	public int delete(BbsVO vo);
-	public BbsVO readOne(BbsVO vo);
+	BbsVO readOne(BbsVO vo);
 }
