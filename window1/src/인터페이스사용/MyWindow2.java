3f4730c65b25bb60068a7f67574d39f8ea888eda
aliciawill <aliciawill@kakao.com>
2022-04-26 10:55:33 +0900

    익명클래스

--- window1/src/인터페이스사용/MyWindow2.java
+++ window1/src/인터페이스사용/MyWindow2.java
@@ -22,7 +22,7 @@ public class MyWindow2 {
 		b1.setFont(font);
 		
 		f.add(b1);
-		b1.setBounds(100, 100, 100, 100);
+		b1.setBounds(50, 100, 200, 100);
 		b1.setForeground(Color.blue);
 		
 		JButton b2 = new JButton();
@@ -30,8 +30,25 @@ public class MyWindow2 {
 		b2.setFont(font);
 		
 		f.add(b2);
-		b2.setBounds(250, 100, 100, 100); //x, y, 가로, 세로 
+		b2.setBounds(260, 100, 200, 100); //x, y, 가로, 세로 
 		b2.setForeground(Color.red);
+		
+		JButton b3 = new JButton();
+		b3.setText("나를 또 눌러요");
+		b3.setFont(font);
+		
+		f.add(b3);
+		b3.setBounds(50, 200, 200, 100);
+		b3.setForeground(Color.blue);
+		
+		JButton b4 = new JButton();
+		b4.setText("나도 또 눌러요");
+		b4.setFont(font);
+		
+		f.add(b4);
+		b4.setBounds(260, 200, 200, 100);
+		b4.setForeground(Color.red);
+		
 		
 		f.setVisible(true);
 	}
